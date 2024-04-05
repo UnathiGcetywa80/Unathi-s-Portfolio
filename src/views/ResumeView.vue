c4ae838e92b849879ae40cf6a44b2a6e824fda1c
UnathiGcetywa80 <unathigcetywa80@gmail.com>
2024-04-05 15:34:03 +0200

    done

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -23,8 +23,8 @@
 
     <!-- Experience Section -->
     <div class="section-box">
+      <h2 class="section-title" id="experience">Experience</h2>
       <section class="experience">
-        <h2 class="section-title" id="experience">Experience</h2>
         <div class="timeline">
           <div v-for="(experienceItem, index) in Experience" :key="index" class="timeline-item" :class="{ 'timeline-item-left': index % 2 === 0, 'timeline-item-right': index % 2 !== 0 }">
             <div class="timeline-content">
@@ -38,83 +38,98 @@
         </div>
       </section>
     </div>
-
-    <!-- Skills Section -->
-<section class="skills">
-  <h2 class="section-title">Skills</h2>
-  <div class="skills-container">
-    <div v-for="skill in Skills" :key="skill.name" class="card">
-      <div class="card-inner">
-        <div class="card-front">
-          <img :src="skill.imgUrl" class="card-img-top" :alt="skill.name" />
-          <h1 class="card-text">{{ skill.name }}</h1>
+    <h4 class="display-4 text-white" id="sub-heading">Skills</h4>
+          <div class="row d-block d-md-flex justify-content-center align-content-end">
+      <div class="col">
+     <div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <img src="https://i.postimg.cc/sxTNXbvK/msql.jpg" alt="HTML" style="width:300px;height:300px;">
         </div>
-        <div class="card-back">
-          <p>{{ skill.text }}</p>
+        <div class="flip-card-back">
+          <img src="https://i.postimg.cc/9MfKyWZZ/html.jpg" alt="CSS" style="width:300px;height:300px;">
         </div>
       </div>
     </div>
   </div>
-</section>
-
+  <div class="col">
+    <div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <img src="https://i.postimg.cc/vZfsmHtm/boot.jpg" alt="BOOTSTRAP" style="width:300px;height:300px;">
+        </div>
+        <div class="flip-card-back">
+          <img src="https://i.postimg.cc/YCWpmxMM/css.webp" alt="JS" style="width:300px;height:300px;">
+         </div>
+        </div>
+    </div>
+    </div>
+     </div>
   </div>
-</template>
+  </template>
 
 <script>
 export default {
   data() {
     return {
-      "Education": [
+      Education: [
         {
-          "degree": "Web Development",
-          "institution":"Life Choices Academy",
-          "description": "Coding",
-          "duration": "2023 - 2024"
-        }, 
+          degree: "Web Development",
+          institution: "Life Choices Academy",
+          description: "Coding",
+          duration: "2023 - 2024"
+        },
         {
-          "degree": "Grade 12",
-          "institution": "HF Van Cutsem High School",
-          "description": "Commerce",
-          "duration": "2018 - 2022"
+          degree: "Grade 12",
+          institution: "HF Van Cutsem High School",
+          description: "Commerce",
+          duration: "2018 - 2022"
         }
       ],
-      "Experience": [
+      Experience: [
         {
-          "place": "Life Choices Academy" ,
-          "duration": "2024-Present",
-          "profession": "Web Dev Intern"
+          place: "Life Choices Academy",
+          duration: "2024-Present",
+          profession: "Web Dev Intern"
         },
         {
-          "place": "Dei Vlei Boodrey" ,
-          "duration": "2022-2022",
-          "profession": "Grape harvester"
+          place: "Dei Vlei Boodrey",
+          duration: "2022-2022",
+          profession: "Grape harvester"
         }
       ],
-      "Skills": [
+      projects: [
         {
-          "name": "HTML",
-          "imgUrl": "https://i.imgur.com/42X0yia.png",
-          "text": "HTML, or Hypertext Markup Language, is a markup language for the web that defines the structure of web pages. It is one of the most basic building blocks of every website, so it's crucial to learn if you want to have a career in web development."
+          title: "Node_EOMP",
+          image: "https://i.ibb.co/84LHFpN/T-J-2.png",
+          github: "https://github.com/niyaazdaniels/NODE_EOMP.git",
+          netlify: "https://nodeeomp-7c094.web.app"
         },
         {
-          "name": "CSS",
-          "imgUrl": "https://i.imgur.com/B4YCCEF.jpg",
-          "text": "CSS stands for Cascading Style Sheets. CSS saves a lot of work. It can control the layout of multiple web pages all at once. What is CSS? Cascading Style Sheets (CSS) is used to format the layout of a webpage.HTML Styles CSS - W3Schools."
+          title: "K's ecommerce",
+          image: "https://i.ibb.co/Jr5jJgP/download-12.jpg",
+          github: "https://github.com/thimnaalam/Khanyile.js.git",
+          netlify: "https://khanyiles.netlify.app/"
         },
         {
-          "name": "Bootstrap",
-          "imgUrl": "https://i.imgur.com/fIcKnQW.png",
-          "text": "As a framework, Bootstrap is a collection of pre-written code chunks in CSS, HTML, and JavaScript that allows developers to create websites more quickly than if they had to create every bit of code from scratch. Bootstrap saves developers time so that they can focus on other things rather than coding CSS."
+          title: "Bootstrap Portfolio",
+          image: "https://i.ibb.co/TMMjCmc/1000-F-474835440-XYky-AZj-LECO54-WGb-BDAu3-Ar9-Bm23kh9-Q.jpg",
+          github: "https://github.com/thimnaalam/BootstrapProject.git",
+          netlify: "https://bootstrapthimna.netlify.app/"
         },
         {
-          "name": "Javascript",
-          "imgUrl": "https://i.imgur.com/zboSh42.jpg",
-          "text": "Together with HTML and CSS, JavaScript forms the foundation of web development. The language is used heavily in web pages, but now various JavaScript platforms allow you to execute JavaScript on a server without a browser. It is also used to create mobile apps for Android and iOS platforms.."
+          title: "Calculator",
+          image: "https://i.ibb.co/nB4mJP0/download-4.png",
+          github: "https://github.com/thimnaalam/projectCalculator.git"
         },
         {
-          "name": "Vuejs",
-          "imgUrl": "https://i.imgur.com/UhHAk6K.png",
-          "text": "Vue.js lets you extend HTML with HTML attributes called directives Vue.js directives offers functionality to HTML applications Vue.js provides built-in directives and user defined directives Vue.js uses double braces { { }} as place-holders for data. In the example below, a new Vue object is created with new Vue ()."
+          title: "Keyboard",
+          image: "https://i.ibb.co/GT5XJ48/5fd7cf7f78a5740019a15561.webp"
+        },
+        {
+          title: "MobiCare",
+          image: "https://i.ibb.co/B6LX3Vq/download-5.png",
+          github: "https://github.com/thimnaalam/MobiCare_week1.git"
         }
       ]
     };
@@ -123,72 +138,71 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.section-title{
-  margin-top: 3%;
-}
+
+
+
 .section-box {
-  width: 70%;
+  width: 80%;
   margin-bottom: 50px;
+  border-radius: 10px; /* Rounded corners */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Shadow for a card effect */
+  padding: 20px;
+  box-align: center;
 }
 
 .section-title {
   margin-bottom: 20px;
+  color: #333; /* Darker text color */
+}
+.section-title{
+  color: white;
+  font-family: 'Lobster', cursive;
 }
 
 .timeline {
   display: flex;
   flex-direction: column;
 }
-
+#sub-heading{
+  font-size: 40px;
+  font-family: 'Lobster', cursive;
+}
 .timeline-item {
   display: flex;
   width: 100%;
   padding: 20px;
-}
-
-.timeline-item-left {
-  justify-content: flex-end;
-}
-
-.timeline-item-right {
-  justify-content: flex-start;
+  border-bottom: 1px solid #050505; /* Separator between items */
 }
 
 .timeline-content {
   display: flex;
   flex-direction: column;
   width: 50%;
+  font-family: 'Lobster', cursive;
+ margin-left: 27%;
 }
 
 .timeline-content h3 {
   margin-bottom: 10px;
 }
 
-.skills {
-  width: 70%;
-}
-
-.skills h2 {
-  margin-top: 50px;
-  margin-bottom: 20px;
-}
-
-.skills-container {
+.card-container {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-  align-items: center;
+  align-items: flex-start;
+}
+.sub-heading{
+  color: white;
 }
 
 .card {
   width: 200px;
-  
-
+  margin-bottom: 20px;
+  background-color: #000; /* Change background color to black */
+  border-radius: 10px; /* Rounded corners */
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1); /* Shadow for a card effect */
+  overflow: hidden; /* Hide overflowing content */
 }
 
 .card-inner {
@@ -213,12 +227,13 @@ export default {
 }
 
 .card-front {
-  background-color: #fff;
+  color: #080808; /* White text color */
+  background-color: transparent; /* Transparent background */
 }
 
 .card-back {
-  color:white;
-  background-color: #f2f2f2;
+  color: #333; /* Darker text color */
+  background-color: #070606; /* Light gray background color */
   transform: rotateY(180deg);
 }
 
@@ -232,28 +247,70 @@ export default {
   margin-top: 20px;
   font-size: 20px;
   font-weight: bold;
+  font-family: 'Lobster', cursive;
 }
 
 .card-back p {
   padding: 20px;
   font-size: 16px;
 }
-.education{
-  background-color: black;
-  margin-top: 10%;
-  border-radius: 25%;
-}
-.experience{
-  background-color: black;
-  margin-top: 10%;
-  border-radius: 25%; 
-}
-#experience{
-  /* margin-top: 30%; */
-}
-.card-text{
-  color: white;
+
+.education,
+.experience {
+  background-color: #333; /* Dark background color */
+  border-radius: 10px; /* Rounded corners */
+  padding: 20px;
+  color: #fff; /* Light text color */
+  margin-top: 20px;
 }
 
-
+#experience {
+  margin-top: 30px;
+}
+.flip-card {
+    background-color: transparent;
+    width: 300px;
+    height: 300px;
+    perspective: 1000px;
+    padding-left: 200px;
+    margin-top: 40px;
+    margin-bottom: 200px;
+  }
+  
+  .flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  }
+  
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+  
+  .flip-card-front, .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+  
+  .flip-card-front {
+    background-color: #bbb;
+    color: black;
+  }
+  
+  .flip-card-back {
+    background-color: #2980b9;
+    color: white;
+    transform: rotateY(180deg);
+  }
+.sub-heading{
+  color: #fff;
+  font-family: 'Lobster', cursive;
+}
 </style>
